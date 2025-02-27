8e05d3892e8273b1ac52a806146f20821d0f42e9
senmu <951967917@qq.com>
2025-02-27 22:57:02 +0800

    chore: 优化合约交互

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,16 @@
 import { useRoutes } from 'react-router-dom';
 import routes from '@routes/index';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   const routing = useRoutes(routes);
   return (
-    <div className="min-h-screen bg-white dark:bg-stone-800 transition-colors duration-200 ease-in">
-      {routing}
-    </div>
+    <>
+      <Toaster position="top-center" />
+      <div className="min-h-screen bg-white dark:bg-stone-800 transition-colors duration-200 ease-in">
+        {routing}
+      </div>
+    </>
   );
 };
 
